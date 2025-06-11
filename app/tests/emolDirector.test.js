0dea741942909c90499a9cd4ef9889da2a858487
Benjamin Ross <benjamn.ross@uc.cl>
2025-06-11 01:21:45 -0400

    fix: improve code formatting and structure in emolDirector tests

--- app/tests/emolDirector.test.js
+++ app/tests/emolDirector.test.js
@@ -1,26 +1,34 @@
 const emolDirector = require('../scrappers/emolDirector');
-const puppeteer = require('puppeteer');
-const sampleData = require('./emolPageInfo.json');
+const puppeteer   = require('puppeteer');
+const sampleData  = require('./emolPageInfo.json');
 const { describe, it, beforeAll, afterAll, expect } = require('@jest/globals');
 
 describe('emolDirector Tests', () => {
-    let browser;
-    let page;
-    let pageInfo;
-    
-    beforeAll(async () => {
-        browser = await puppeteer.launch({ headless: true });
-        page = await browser.newPage();
-        await page.goto('https://www.emol.com/noticias/Nacional/2025/06/10/1168945/senado-multas-por-no-votar.html');
-        const director = new emolDirector(page);
-        pageInfo = await director.buildPageInfo();
+  let browser;
+  let page;
+  let pageInfo;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ]
     });
-    
-    afterAll(async () => {
-        await browser.close();
-    });
-    
-    it('should build page info correctly', async () => {
-        expect(pageInfo).toEqual(sampleData);
-    });
+    page = await browser.newPage();
+    await page.goto('https://www.emol.com/noticias/Nacional/2025/06/10/1168945/senado-multas-por-no-votar.html'); 
+    const director = new emolDirector(page);
+    pageInfo = await director.buildPageInfo();
+  });
+
+  afterAll(async () => {
+    if (browser) {
+      await browser.close();
+    }
+  });
+
+  it('should build page info correctly', async () => {
+    expect(pageInfo).toEqual(sampleData);
+  });
 });
